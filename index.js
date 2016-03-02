c93a97a2a773971b1f08440a02ec478e1dbf083d
Venkatesh <inventvenkat@gmail.com>
2016-03-02 11:55:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var http = require("http");
-http.createServer(function (request, response) {
+http.set('port', (process.env.PORT || 5000));
 
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('venkatesh\nSathish');
+http.use(express.static(__dirname + '/public'));
+http.set('views', __dirname + '/views');
+http.set('view engine', 'ejs');
 
-}).listen(8081);
+http.get('/',function(request,response){
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+   	response.end('venkatesh\nSathish');
+});
 
-console.log('Server running at http://127.0.0.1:8081/');
+http.listen(http.get('port'), function() {
+  console.log('Node http is running on port', http.get('port'));
+});
