c5cd4a4977d326345ba26f16ec891e713d635dde
Venkatesh <inventvenkat@gmail.com>
2016-03-02 14:10:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var http = require("http");
 http.set('port', (process.env.PORT || 5000));
-
-http.use(express.static(__dirname + '/public'));
-http.set('views', __dirname + '/views');
-http.set('view engine', 'ejs');
-
 http.get('/',function(request,response){
 	response.writeHead(200, {'Content-Type': 'text/plain'});
    	response.end('venkatesh\nSathish');
